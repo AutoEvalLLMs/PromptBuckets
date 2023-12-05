75229f1531239f5f7817f57521ac1fcdddc8f9ee
Ubuntu <ubuntu@ip-172-31-23-218.ec2.internal>
2023-12-05 05:34:40 +0000

    push

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,21 @@
 from genbit.genbit_metrics import GenBitMetrics
+import csv
+import evaluate
 
-class GenBitEvaluator():
+class AutoEvalEvaluator:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def load_prompts_csv(prompt_fp):
+        with open(prompt_fp, "r") as f:
+            ret = [p[-1] for p in csv.reader(f)]
+
+    def evaluate():
+        raise NotImplementedError()
+
+
+class GenBitWrapper():
     def __init__(self):
         """
         Genbit Evaluator
@@ -18,3 +33,28 @@ class GenBitEvaluator():
 
     def __call__(self, string):
         self.add_data(string)
+
+
+class GenBitEvaluator(AutoEvalEvaluator):
+    def __init__(self, prompts_fp):
+        self.prompts = AutoEvalEvaluator.load_prompts_csv(prompts_fp)
+        self.genbit = GenBitWrapper()
+
+    def evaluate(self, targetLLM):
+        for p in self.prompts:
+            self.genbit(targetLLM.generate(p))
+
+        return self.genbit.evaluate()
+
+
+class HonestEvaluator(AutoEvalEvaluator):
+    def __init__(self, prompts_fp):
+        self.prompts = AutoEvalEvaluator.load_prompts_csv(prompts_fp)
+        self.honest = evaluate.load("honest", "en")
+
+    def evaluate(self, targetLLM):
+        generations = [targetLLM.generate(p) for p in self.prompts]
+        return self.honest.compute(predictions=generations)
+
+
+
